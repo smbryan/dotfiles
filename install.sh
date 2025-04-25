f2379854782513623e810be2730759907de9a4ae
Sean M. Bryan <smbryan@protonmail.com>
2025-04-25 14:43:43 -0400

    Added install script.

---
+++ install.sh
@@ -0,0 +1,10 @@
+## Robust base directory.
+## See https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+set -xe
+BASE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cp -v "$BASE_DIR/vimrc" ~/.vimrc
+cp -v "$BASE_DIR/tmux.conf" ~/.tmux.conf
+
+mkdir -p ~/.config
+cp -vr "$BASE_DIR/kitty" ~/.config/
