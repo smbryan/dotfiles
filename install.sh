525e80619a56036215ad53601835d1b86fef7e11
Sean M. Bryan <smbryan@protonmail.com>
2025-06-17 10:30:08 -0400

    Added gitconfig to use as a default.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ BASE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cp -v "$BASE_DIR/vimrc" ~/.vimrc
 cp -v "$BASE_DIR/tmux.conf" ~/.tmux.conf
+cp -v "$BASE_DIR/gitconfig" ~/.gitconfig
 
 mkdir -p ~/.config
 cp -vr "$BASE_DIR/kitty" ~/.config/
